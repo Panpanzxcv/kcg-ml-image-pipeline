fffd2e9f8735a879c3e72cdea0649776f662cb15
Senyoret1 <34079003+Senyoret1@users.noreply.github.com>
2024-08-13 17:19:38 -0400

    Uuid improvements

--- orchestration/api/mongo_schema/extracts_schemas.py
+++ orchestration/api/mongo_schema/extracts_schemas.py
@@ -1,6 +1,8 @@
 from typing import List
 import uuid
 
+from orchestration.api.api_utils import uuid64_number_to_string
+
 class ExtractsHelpers():
     @staticmethod
     def clean_extract_for_api_response(data: dict):
@@ -8,6 +10,10 @@ class ExtractsHelpers():
         if "uuid" in data:
             if isinstance(data['uuid'], uuid.UUID):
                     data['uuid'] = str(data['uuid'])
+
+        if "image_uuid" in data:
+            if isinstance(data['image_uuid'], int):
+                    data['image_uuid'] = uuid64_number_to_string(data['image_uuid'])
 
     @staticmethod
     def clean_extract_list_for_api_response(data_list: List[dict]):
