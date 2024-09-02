2afc471862922c8e34a7f267608c56bfa254626b
sandrodevdariani <sdevdariani9@gmail.com>
2024-09-02 12:45:21 +0400

    update script

--- scripts/clean_collections_from_deleted_images.py
+++ scripts/clean_collections_from_deleted_images.py
@@ -2,16 +2,17 @@ from pymongo import MongoClient
 from minio import Minio
 from minio.error import S3Error
 
-def process_collection(collection, minio_client, db, hash_field):
+def process_collection(collection_name, minio_client, db, hash_field):
     """
     Iterate through each document in the collection and remove it if the hash does not exist
     in the completed-jobs, extracts, or external_images collections.
     """
+    collection = db[collection_name]
     orphaned_count = 0
 
-    print(f"Starting to process collection: {collection.name}")
+    print(f"Starting to process collection: {collection_name}")
     total_docs = collection.count_documents({})
-    print(f"Total documents in {collection.name}: {total_docs}")
+    print(f"Total documents in {collection_name}: {total_docs}")
     
     cursor = collection.find({}, {hash_field: 1})
     processed_docs = 0
@@ -43,7 +44,7 @@ def process_collection(collection, minio_client, db, hash_field):
             file_path = doc.get("file_path") or doc.get("task_output_file_dict", {}).get("output_file_path")
             if file_path:
                 print(f"Processing orphaned document with file_path: {file_path}")
-                if collection.name == "all-images":
+                if collection_name == "all-images":
                     try:
                         bucket_name, object_name = file_path.split('/', 1)
                         delete_files_from_minio(minio_client, bucket_name, object_name)
@@ -54,8 +55,8 @@ def process_collection(collection, minio_client, db, hash_field):
             collection.delete_one({"_id": doc["_id"]})
             print(f"Removed orphaned document with _id: {doc['_id']} and {hash_field}: {doc_hash}")
 
-    print(f"Processed {processed_docs} documents in {collection.name}")
-    print(f"Total orphaned documents removed from {collection.name}: {orphaned_count}")
+    print(f"Processed {processed_docs} documents in {collection_name}")
+    print(f"Total orphaned documents removed from {collection_name}: {orphaned_count}")
 
 def delete_files_from_minio(minio_client, bucket_name, object_name):
     files_to_delete = [
@@ -94,26 +95,27 @@ def main():
     )
 
     collections_to_remove = [
-        db.image_tags_collection,
-        db.all_image_collection,
-        db.image_rank_scores_collection,
-        db.image_classifier_scores_collection,
-        db.image_sigma_scores_collection,
-        db.image_residuals_collection,
-        db.image_percentiles_collection,
-        db.image_residual_percentiles_collection,
-        db.image_rank_use_count_collection,
-        db.image_pair_ranking_collection,
-        db.irrelevant_images_collection,
-        db.image_hashes_collection,
+        "image_tags",
+        "all-images",
+        "image_rank_scores",
+        "image_classifier_scores",
+        "image-sigma-scores",
+        "image-residuals",
+        "image-percentiles",
+        "image-residual-percentiles",
+        "image-rank-use-coun",
+        "image_pair_ranking",
+        "irrelevant_images",
+        "image_hashes",
+        "ranking_datapoints"
     ]
 
-    for collection in collections_to_remove:
+    for collection_name in collections_to_remove:
         hash_field = "image_hash"
-        if collection.name == "irrelevant_images_collection":
+        if collection_name == "irrelevant_images":
             hash_field = "file_hash"
 
-        process_collection(collection, minio_client, db, hash_field)
+        process_collection(collection_name, minio_client, db, hash_field)
 
     print("Cleanup completed.")
 
